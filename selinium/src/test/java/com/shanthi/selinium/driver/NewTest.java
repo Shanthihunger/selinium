cc938cee51bf8a0c89dfa27ac6d67be1e3c17343
shanthihunger <shanthi@9611356248>
2023-03-13 17:22:14 +0530

    first commit

---
+++ selinium/src/test/java/com/shanthi/selinium/driver/NewTest.java
@@ -0,0 +1,9 @@
+package com.shanthi.selinium.driver;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+  }
+}
---
+++ selinium/src/test/java/com/shanthi/selinium/package-infoTest.java
@@ -0,0 +1,11 @@
+package com.shanthi.selinium;
+
+import org.testng.annotations.Test;
+
+public class package-infoTest {
+
+  @Test
+  public void mainTest() {
+    throw new RuntimeException("Test not implemented");
+  }
+}
